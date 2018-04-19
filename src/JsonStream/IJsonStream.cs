60acd393171991e44540e5bc3876ac008aef8402
mvleandro <mleandro@stone.com.br>
2018-04-19 13:24:31 -0300

    Adding async methods skeleton.

--- src/JsonStream/IJsonStream.cs
+++ src/JsonStream/IJsonStream.cs
@@ -1,8 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace StoneCo.Utils.IO
 {
-    public interface IJsonStream
+    interface IJsonStream
     {
         /// <summary>
         /// Returns the bytes of the document and move to the next document.
@@ -11,10 +12,22 @@ namespace StoneCo.Utils.IO
         byte[] ReadBytes();
 
         /// <summary>
+        /// Returns the bytes of the document and move to the next document.
+        /// </summary>
+        /// <returns>The bytes of the document.</returns>
+        Task<byte[]> ReadBytesAsync();
+
+        /// <summary>
         /// Returns a JArray and move to the next document.
         /// </summary>
         /// <returns>The JArray.</returns>
         JArray ReadJArray();
+
+        /// <summary>
+        /// Returns a JArray and move to the next document.
+        /// </summary>
+        /// <returns>The JArray.</returns>
+        Task<JArray> ReadJArrayAsync();
 
         /// <summary>
         /// Returns a JObject and move to the next document.
@@ -23,10 +36,22 @@ namespace StoneCo.Utils.IO
         JObject ReadJson();
 
         /// <summary>
+        /// Returns a JObject and move to the next document.
+        /// </summary>
+        /// <returns>The JObject.</returns>
+        Task<JObject> ReadJsonAsync();
+
+        /// <summary>
         /// Returns a JToken and move to the next document.
         /// </summary>
         /// <returns>The JToken.</returns>
         JToken ReadJToken();
+
+        /// <summary>
+        /// Returns a JToken and move to the next document.
+        /// </summary>
+        /// <returns>The JToken.</returns>
+        Task<JToken> ReadJTokenAsync();
 
         /// <summary>
         /// Returns an instance of T and move to the next document.
@@ -36,10 +61,23 @@ namespace StoneCo.Utils.IO
         T ReadObject<T>();
 
         /// <summary>
+        /// Returns an instance of T and move to the next document.
+        /// </summary>
+        /// <typeparam name="T">The instance type.</typeparam>
+        /// <returns>The T instance.</returns>
+        Task<T> ReadObjectAsync<T>();
+
+        /// <summary>
         /// Returns the string of the document and move to the next document.
         /// </summary>
         /// <returns>The string of the document.</returns>
         string ReadString();
+
+        /// <summary>
+        /// Returns the string of the document and move to the next document.
+        /// </summary>
+        /// <returns>The string of the document.</returns>
+        Task<string> ReadStringAsync();
 
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
@@ -51,8 +89,21 @@ namespace StoneCo.Utils.IO
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
         /// </summary>
+        /// <param name="bytes">The bytes of the document.</param>
+        /// <param name="validate">Throws an exception if the bytes is not a valid Json document.</param>
+        Task WriteBytesAsync(byte[] bytes, bool validate = true);
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
         /// <param name="jArray">The JArray to write.</param>
         void WriteJArray(JArray jArray);
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
+        /// <param name="jArray">The JArray to write.</param>
+        Task WriteJArrayAsync(JArray jArray);
 
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
@@ -63,8 +114,20 @@ namespace StoneCo.Utils.IO
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
         /// </summary>
+        /// <param name="jObject">The JObject to write.</param>
+        Task WriteJsonAsync(JObject jObject);
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
         /// <param name="jToken">The JToken to write.</param>
         void WriteJToken(JToken jToken);
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
+        /// <param name="jToken">The JToken to write.</param>
+        Task WriteJTokenAsync(JToken jToken);
 
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
@@ -76,8 +139,22 @@ namespace StoneCo.Utils.IO
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
         /// </summary>
+        /// <param name="jString">The string of the document.</param>
+        /// <param name="validate">Throws an exception if the bytes is not a valid Json document.</param>
+        Task WriteStringAsync(string jString, bool validate = true);
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
         /// <typeparam name="T">The instance type.</typeparam>
         /// <param name="instance">The instance of T to write.</param>
         void WriteObject<T>(T instance);
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
+        /// <typeparam name="T">The instance type.</typeparam>
+        /// <param name="instance">The instance of T to write.</param>
+        Task WriteObjectAsync<T>(T instance);
     }
 }
--- src/JsonStream/JsonStream.cs
+++ src/JsonStream/JsonStream.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
 namespace StoneCo.Utils.IO
 {
     public class JsonStream : IJsonStream, IDisposable
     {
-
         #region Constants
 
         /// <summary>
@@ -124,10 +124,28 @@ namespace StoneCo.Utils.IO
         }
 
         /// <summary>
+        /// Returns the bytes of the document and move to the next document.
+        /// </summary>
+        /// <returns>The bytes of the document.</returns>
+        public async Task<byte[]> ReadBytesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// Returns a JArray and move to the next document.
         /// </summary>
         /// <returns>The JArray.</returns>
         public JArray ReadJArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a JArray and move to the next document.
+        /// </summary>
+        /// <returns>The JArray.</returns>
+        public async Task<JArray> ReadJArrayAsync()
         {
             throw new NotImplementedException();
         }
@@ -142,10 +160,28 @@ namespace StoneCo.Utils.IO
         }
 
         /// <summary>
+        /// Returns a JObject and move to the next document.
+        /// </summary>
+        /// <returns>The JObject.</returns>
+        public async Task<JObject> ReadJsonAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// Returns a JToken and move to the next document.
         /// </summary>
         /// <returns>The JToken.</returns>
         public JToken ReadJToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a JToken and move to the next document.
+        /// </summary>
+        /// <returns>The JToken.</returns>
+        public async Task<JToken> ReadJTokenAsync()
         {
             throw new NotImplementedException();
         }
@@ -161,10 +197,29 @@ namespace StoneCo.Utils.IO
         }
 
         /// <summary>
+        /// Returns an instance of T and move to the next document.
+        /// </summary>
+        /// <typeparam name="T">The instance type.</typeparam>
+        /// <returns>The T instance.</returns>
+        public async Task<T> ReadObjectAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// Returns the string of the document and move to the next document.
         /// </summary>
         /// <returns>The string of the document.</returns>
         public string ReadString()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns the string of the document and move to the next document.
+        /// </summary>
+        /// <returns>The string of the document.</returns>
+        public async Task<string> ReadStringAsync()
         {
             throw new NotImplementedException();
         }
@@ -182,8 +237,27 @@ namespace StoneCo.Utils.IO
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
         /// </summary>
+        /// <param name="bytes">The bytes of the document.</param>
+        /// <param name="validate">Throws an exception if the bytes is not a valid Json document.</param>
+        public async Task WriteBytesAsync(byte[] bytes, bool validate = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
         /// <param name="jArray">The JArray to write.</param>
         public void WriteJArray(JArray jArray)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
+        /// <param name="jArray">The JArray to write.</param>
+        public async Task WriteJArrayAsync(JArray jArray)
         {
             throw new NotImplementedException();
         }
@@ -200,8 +274,26 @@ namespace StoneCo.Utils.IO
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
         /// </summary>
+        /// <param name="jObject">The JObject to write.</param>
+        public async Task WriteJsonAsync(JObject jObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
         /// <param name="jToken">The JToken to write.</param>
         public void WriteJToken(JToken jToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
+        /// <param name="jToken">The JToken to write.</param>
+        public async Task WriteJTokenAsync(JToken jToken)
         {
             throw new NotImplementedException();
         }
@@ -219,9 +311,29 @@ namespace StoneCo.Utils.IO
         /// <summary>
         /// Writes a document and move the pointer to the end of file.
         /// </summary>
+        /// <param name="jString">The string of the document.</param>
+        /// <param name="validate">Throws an exception if the bytes is not a valid Json document.</param>
+        public async Task WriteStringAsync(string jString, bool validate = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
         /// <typeparam name="T">The instance type.</typeparam>
         /// <param name="instance">The instance of T to write.</param>
         public void WriteObject<T>(T instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a document and move the pointer to the end of file.
+        /// </summary>
+        /// <typeparam name="T">The instance type.</typeparam>
+        /// <param name="instance">The instance of T to write.</param>
+        public async Task WriteObjectAsync<T>(T instance)
         {
             throw new NotImplementedException();
         }
